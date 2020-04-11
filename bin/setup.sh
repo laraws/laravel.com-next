389229bc8fbe145a3249e3397dcd10bf49288747
hasee-linux <larawsw@outlook.com>
2020-04-11 23:23:39 +0800

    feat: add docker config and build project

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,9 +5,13 @@ if [ ! -f composer.json ]; then
     exit 1
 fi
 
-composer install
+#composer install
+docker run --rm -v $PWD:/app composer install
 cp .env.example .env
-php artisan key:generate
+#php artisan key:generate
+docker run --rm -v $PWD:/app laraws/php php artisan key:generate
 source "$(dirname "$0")/checkout_latest_docs.sh"
-npm install
-npm run dev
+#npm install
+docker run --rm -v $PWD:/app node -w /app npm install
+#npm run dev
+docker run --rm -v $PWD:/app node npm -w/app run dev
